8fde90842ea0bafe9d748b12c406e34e410ac8a1
Jibachh Yadav <jibachhydv18@gmail.com>
2020-10-17 08:45:38 +0545

    modification on homepage

--- users/views.py
+++ users/views.py
@@ -21,15 +21,33 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.contrib.sites.shortcuts import get_current_site
 from django.contrib.auth import get_user_model
 
-
+from note.models import *
+from discussion.models import Discussion
 # from .forms import SignUpForm
 
+
 def homepage(request):
-    return render(request, 'base.html')
+
+    # Reccommended Posts
+    if request.user.is_authenticated:
+        recNotes = Note.published.all().exclude(
+            author=request.user).order_by('-views', 'likes')[:4]
+    else:
+        recNotes = Note.published.all()[:4]
+    print(recNotes)
+
+    # Recommended Question
+    recQuestions = Discussion.objects.all().exclude(answered_status=True)[:4]
+    print(recQuestions)
+
+    return render(request, 'base.html', {
+        'recommendedNotes': recNotes,
+        'recommendedQuestion': recQuestions,
+    })
 
 
 def login(request):
-   
+
     if request.method == "POST":
         email = request.POST.get('email')
         password = request.POST.get('password')
@@ -37,10 +55,10 @@ def login(request):
         user = authenticate(request, email=email, password=password)
         if user is not None:
             if user.is_active:
-                account_login(request,user)
+                account_login(request, user)
                 return HttpResponseRedirect(reverse('homepage'))
             else:
-                return render(request, 'account/account_not_activated.html' , {
+                return render(request, 'account/account_not_activated.html', {
 
                 })
         else:
@@ -52,6 +70,7 @@ def login(request):
             return HttpResponseRedirect(reverse('homepage'))
         return render(request, "account/login.html")
 
+
 def logout(request):
     account_logout(request)
     return HttpResponseRedirect(reverse("homepage"))
@@ -62,40 +81,50 @@ class PasswordChangeView(PasswordChangeView):
     template_name = 'account/passwordchange.html'
 
 # Password Change Done
+
+
 class PasswordChangeDoneView(PasswordChangeDoneView):
     template_name = 'account/passwordchangedone.html'
 
 # Password reset view
+
+
 class PasswordResetView(PasswordResetView):
     template_name = 'account/password_reset_email.html'
 
 # Password Reset Done View
+
+
 class PasswordResetDoneView(PasswordResetDoneView):
     template_name = 'account/password_reset_done.html'
+
 
 class PasswordResetConfirmView(PasswordResetConfirmView):
     template_name = 'account/password_reset_confirm.html'
     extra_context = {
-        
+
     }
+
 
 class PasswordResetCompleteView(PasswordResetCompleteView):
     template_name = 'account/password_reset_complete.html'
 
 
 # Profile
-def profile(request,id, username):
-    
+def profile(request, id, username):
+
     try:
         user = User.objects.get(pk=id, username=username)
     except User.DoesNotExist:
         return HttpResponse("No Such User")
-    
+
     return render(request, 'profile/profile.html', {
         'user': user
     })
-    
+
 # User registration
+
+
 def signup(request):
 
     if request.user.is_authenticated:
@@ -105,7 +134,7 @@ def signup(request):
         return render(request, 'registration/signup.html')
 
     if request.method == 'POST':
-        email_address = request.POST["email"] 
+        email_address = request.POST["email"]
         first_name = request.POST['firstName']
         last_name = request.POST['lastName']
         password1 = request.POST['password1']
@@ -116,7 +145,7 @@ def signup(request):
         else:
             havePhoto = False
         # photo = request.POST['profilePicture']
-        
+
         # Ensure that password matches
         if len(password1) < 8 or len(password2) < 8:
             return render(request, 'registration/signup.html', {
@@ -128,35 +157,34 @@ def signup(request):
                 "error_message": "Passwords don't match",
             })
 
-        
         # Attempt to create a new user
         try:
             if havePhoto:
-                user = User.objects.create_user(username=username, 
-                        first_name=first_name, last_name=last_name, 
-                        email=email_address, password=password2, 
-                        photo=request.POST['profilePicture'])
+                user = User.objects.create_user(username=username,
+                                                first_name=first_name, last_name=last_name,
+                                                email=email_address, password=password2,
+                                                photo=request.POST['profilePicture'])
             else:
-                user = User.objects.create_user(username=username, 
-                        first_name=first_name, last_name=last_name, 
-                        email=email_address, password=password2)
+                user = User.objects.create_user(username=username,
+                                                first_name=first_name, last_name=last_name,
+                                                email=email_address, password=password2)
             user.is_active = False
             user.save()
-            
+
         except IntegrityError as e:
             print(e)
             return render(request, 'registration/signup.html', {
                 "error_message": "Email Address Already Taken"
             })
-        
+
         current_site = get_current_site(request)
         mail_subject = 'Activate your account.'
         message = render_to_string('registration/acc_active_email.html', {
-                'user': user,
-                'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                'token': default_token_generator.make_token(user),
-            })
+            'user': user,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+            'token': default_token_generator.make_token(user),
+        })
         to_email = email_address
         email = EmailMessage(
             mail_subject, message, to=[to_email]
@@ -166,7 +194,9 @@ def signup(request):
 
         })
 
+
 UserModel = get_user_model()
+
 
 def activate(request, uidb64, token):
     try:
