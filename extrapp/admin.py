71afc35cce707079e528e6aadcedfe286ec58e89
Jibachh Yadav <jibachhydv18@gmail.com>
2020-10-18 09:27:52 +0545

    read page dynamicity added

--- extrapp/admin.py
+++ extrapp/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import CurrentBook, Participiant
 
+
 # Register your models here.
 admin.site.register(CurrentBook)
 admin.site.register(Participiant)
--- extrapp/views.py
+++ extrapp/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from .models import CurrentBook, Participiant
 
 from datetime import datetime
-
+import calendar
 
 # Create your views here.
 def readNepal(request):
@@ -12,11 +12,16 @@ def readNepal(request):
     currentMonth = datetime.now().month
 
     # Book Of the Month
-    bookOfTheMonth = CurrentBook.objects.all().filter(date__month=currentMonth)[:2]
+    bookOfTheMonth = CurrentBook.objects.all().filter(date__month=currentMonth).filter(isItCurrentBook=True)[:2]
+
+    # Participiant
+    participiants = Participiant.objects.all().filter(dateStarted__month=currentMonth)
 
     # rendering readnepal.html
     return render(request, 'readNepal/readnepal.html', {
-      'books': bookOfTheMonth,  
+      'books': bookOfTheMonth,
+      'monthName': calendar.month_name[currentMonth],
+      'readers': participiants,
     })
 
 def testHomePage(request):
